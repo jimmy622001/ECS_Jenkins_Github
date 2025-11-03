b2b2cd2d3fb788b48a25a6f410642d0b6a398bb6
Developer <dev@example.com>
2025-11-03 10:51:49 +0200

    Add missing variables for monitoring module

--- modules/monitoring/variables.tf
+++ modules/monitoring/variables.tf
@@ -87,3 +87,21 @@ variable "domain_name" {
   description = "Domain name for the application"
   type        = string
 }
+
+variable "enable_prometheus" {
+  description = "Whether to enable Prometheus"
+  type        = bool
+  default     = true
+}
+
+variable "enable_grafana" {
+  description = "Whether to enable Grafana"
+  type        = bool
+  default     = true
+}
+
+variable "additional_tags" {
+  description = "Additional tags for the monitoring resources"
+  type        = map(string)
+  default     = {}
+}
